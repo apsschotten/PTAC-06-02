4d8c13d9e6c1399ed4e08487173f955be2a6ea3b
apsschotten <161073410+apsschotten@users.noreply.github.com>
2024-03-12 22:54:15 -0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,27 @@ export default function Header(){
 
     return(
     <header>
-        <h1>Isto é um Header.</h1>
+        <nav class="navbar">
+            <div class="navbar-header">
+                <img src="image-removebg-preview.png" alt="logo" id="image">
+                <div class="logo">Magicats.com</div>
+            </div>
+    
+            <div class="container">
+                <div class="search-container">
+                  <form action="#" method="get">
+                    <input type="text" placeholder="Pesquisar..." name="search">
+                    <button type="submit">Buscar</button>
+                  </form>
+                </div>
+              </div>
+
+            <ul class="menu" id="menu">
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Minha Conta</a></li>
+                <li><a href="#">Sobre Nós</a></li>
+            </ul>
+        </nav>
     </header>
     );
 }
