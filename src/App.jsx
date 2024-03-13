c049c5b12013942a74a347a21ebabb9d24963c3e
apsschotten <161073410+apsschotten@users.noreply.github.com>
2024-03-12 22:57:49 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./Components/Header.jsx";
 import Footer from "./Components/Footer.jsx";
-import style from "./App.css";
+import Style from "./App.css";
 
 export default function App(){
   return(
