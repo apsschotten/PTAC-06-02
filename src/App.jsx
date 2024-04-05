38804c17e574dc8c3bb9aa99e46a27a66e8dd7bb
apsschotten <ana.schotten@estudante.ifms.edu.br>
2024-04-05 15:44:17 -0400

    Aula 05.04

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header.jsx";
 import Footer from "./Components/Footer.jsx";
+import Main from "./Components/Main.jsx";
 import "./Globals.css";
 
 export default function App(){
   return(
     <main>
       <Header/>
+      <Main/>
       <Footer/>
     </main>
   );
---
+++ src/Components/Main.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Main() {
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState(0);
+  const [listaCtts, setListaCtts] = useState([]);
+
+  const registrar = (event) => {
+    event.preventDefault();
+    alert("Pronto!");
+    setListaCtts([...listaCtts,
+    {
+      nomeSalvo: nome,
+      telefoneSalvo: telefone
+    }
+    ])
+  };
+  console.table(listaCtts);
+
+  return (
+    <main>
+      <form action="" onSubmit={registrar}>
+        <label htmlFor="nome">Nome:</label>
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(event) => setNome(event.target.value)}
+        />
+        {nome}
+        <br></br>
+        <label htmlFor="telefone">Telefone:</label>
+        <input
+          type="tel"
+          name=""
+          id=""
+          onChange={(event) => (event.target.value)} />
+        <button>Salvar</button>
+      </form>
+      {listaCtts.map((contato, index) =>
+        <div key={index}>
+          <p>{contato.nomeSalvo}</p>
+          <p>{contato.telefoneSalvo}</p>
+        </div>)}
+    </main>
+  )
+
+}
