d82ba6079a226aa1d37abbe28c8bf08c04f55e2c
apsschotten <161073410+apsschotten@users.noreply.github.com>
2024-04-05 11:53:02 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { useState } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default function Main() {
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState(0);
+  const [listaCtts, setListaCtts] = useState([]);
+
+  const registrar = (event) => {
+    event.preventDefault();
+    alert("Pronto!");
+    setListaCtts([...listaCtts,
+    {
+      nomeSalvo: nome,
+      telefoneSalvo: telefone
+    }
+    ])
+  };
+  console.table(listaCtts);
+
+  return (
+    <main>
+      <form action="" onSubmit={registrar}>
+        <label htmlFor="nome">Nome:</label>
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(event) => setNome(event.target.value)}
+        />
+        {nome}
+        <br></br>
+        <label htmlFor="telefone">Telefone:</label>
+        <input
+          type="tel"
+          name=""
+          id=""
+          onChange={(event) => (event.target.value)} />
+        <button>Salvar</button>
+      </form>
+    </main>
+  )
+}
